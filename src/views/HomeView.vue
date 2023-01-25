0efb96b66f30394eeb6ffa004a108478a62d5404
flyinacres <ron.fischer@scienceexchange.com>
2023-01-25 00:26:15 -0800

    Moving to composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,22 +10,27 @@
 </template>
 
 <script>
+import {ref} from 'vue'
+
 export default {
-	data() {
-		return {
-			counter: 0
-		}
-	},
-	methods: {
-		increaseCounter() {
-			this.counter++
-		},
-		decreaseCounter() {
-			this.counter--
+	setup() {
+		const counter = ref(0)
+
+		const increaseCounter = () => {
+			counter.value++
 		}
 
+		const decreaseCounter = () => {
+			counter.value--
+		}
+
+		return {
+			counter, increaseCounter, decreaseCounter
+		}
 	}
 }
+
+
 </script>
 
 <style scoped>
