43980ffb109508e81fee5594718db5d7b2f1a825
flyinacres <ron.fischer@scienceexchange.com>
2023-01-25 08:55:28 -0800

    refs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 
 <template>
 	<div class="home">
+		<h3>{{ counterTitle }}:</h3>
 		<button @click="decreaseCounter" class="btn">-</button>
 		<span class="counter">{{counter}}</span>
 		<button @click="increaseCounter" class="btn">+</button>
 
-
+		<div class="edit">
+			<h4>Edit Counter Title:</h4>
+			<input v-model="counterTitle" type="text"/>
+		</div>
 	</div>
+
 </template>
 
 <script setup>
 import {ref} from 'vue'
 
-const counter = ref(0)
+const counter = ref(0),
+	counterTitle = ref('My Counter')
 
 const increaseCounter = () => {
 	counter.value++
@@ -32,6 +38,10 @@ const decreaseCounter = () => {
 .btn, .counter {
 	font-size: 40px;
 	margin: 10px;
+}
+
+.edit {
+	margin-top: 60px;
 }
 
 </style>
