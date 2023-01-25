06b5eae2ca2a694292c19330a9d1c0bcdec2517d
flyinacres <ron.fischer@scienceexchange.com>
2023-01-25 09:00:13 -0800

    non-reactive data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 
 <template>
 	<div class="home">
+		<h2>{{ appTitle }}</h2>
 		<h3>{{ counterData.title }}:</h3>
 		<button @click="decreaseCounter" class="btn">-</button>
 		<span class="counter">{{counterData.count}}</span>
@@ -19,6 +20,8 @@ import {ref, reactive} from 'vue'
 
 const counter = ref(0),
 	counterTitle = ref('My Counter')
+
+const appTitle = "My Amazing Counter App"
 const counterData = reactive({
 	count: 0,
 	title: "My Counter"
