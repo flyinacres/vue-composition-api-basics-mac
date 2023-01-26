25cd24b2a467adf21f442b617a14f55f277c06d8
flyinacres <ron-github@rfgf.com>
2023-01-25 18:00:05 -0800

    Accessing router info programmatically

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -4,11 +4,20 @@
 			This is a post page
 		</h1>
 		<p>Proident sint enim voluptate pariatur labore tempor laboris ut officia. Dolore magna laborum nostrud exercitation et aliqua qui commodo occaecat laborum aliquip laboris. Commodo ipsum est nostrud in mollit id ea enim ex incididunt tempor sit. Proident pariatur Lorem ad anim minim adipisicing et in eu adipisicing nisi sint. Sint qui nulla in aliqua elit anim laborum est. Lorem aliqua consectetur ullamco eiusmod.</p>
-		<p> id is {{  $route.params.id  }}</p>
-		<RouterLink to="/posts">&lt Back </RouterLink>
+		<div>
+			<button @click="showPostId">Show Post Id</button>
+		</div>
+		<p><RouterLink to="/posts">&lt; Back </RouterLink></p>
 	</div>
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router'
 
+// Standard way to access the route
+const route = useRoute()
+
+const showPostId = () => {
+	alert(`The ID of this post is: ${ route.params.id }`)
+}
 </script>
