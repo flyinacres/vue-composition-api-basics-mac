1bd498e48caca3215aad17c47a5248f0775650d6
flyinacres <ron-github@rfgf.com>
2023-01-25 17:54:13 -0800

    Fake posts details

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -1,9 +1,11 @@
 <template>
 	<div class="post-detail">
 		<h1>
-			Posts
+			This is a post page
 		</h1>
-		
+		<p>Proident sint enim voluptate pariatur labore tempor laboris ut officia. Dolore magna laborum nostrud exercitation et aliqua qui commodo occaecat laborum aliquip laboris. Commodo ipsum est nostrud in mollit id ea enim ex incididunt tempor sit. Proident pariatur Lorem ad anim minim adipisicing et in eu adipisicing nisi sint. Sint qui nulla in aliqua elit anim laborum est. Lorem aliqua consectetur ullamco eiusmod.</p>
+		<p> id is {{  $route.params.id  }}</p>
+		<RouterLink to="/posts">&lt Back </RouterLink>
 	</div>
 </template>
 
