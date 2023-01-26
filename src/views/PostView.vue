f8fccf85bc59e902feb458b5dbc5116017f8af07
flyinacres <ron-github@rfgf.com>
2023-01-26 09:53:51 -0800

    v-for

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -4,14 +4,8 @@
 			Posts
 		</h1>
 		<ul>
-			<li>
-				<RouterLink to="/postDetail/id1">Post 1</RouterLink>
-			</li>
-			<li>
-				<RouterLink to="/postDetail/id2">Post 2</RouterLink>
-			</li>
-			<li>
-				<RouterLink to="/postDetail/id3">Post 3</RouterLink>
+			<li v-for="post in posts" :key="post.id">
+				<RouterLink :to="`/postDetail/${ post.id }`">{{ post.title }}</RouterLink>
 			</li>
 		</ul>
 		<textarea v-autofocus/>
@@ -20,6 +14,22 @@
 
 <script setup>
 import { vAutofocus } from '@/directives/vAutofocus'
+import { ref } from 'vue'
+
+const posts = ref([
+	{
+	id: 'id1',
+	title: 'Post 1'
+	},
+	{
+	id: 'id2',
+	title: 'Post 2'
+	},
+	{
+	id: 'id3',
+	title: 'Post 3'
+	}
+])
 </script>
 
 <style scoped>
