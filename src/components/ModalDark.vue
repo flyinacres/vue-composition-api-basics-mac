9101ae4879b15820ed1f5dd94d1cd98b0b829ac9
flyinacres <ron-github@rfgf.com>
2023-01-26 22:27:48 -0800

    Dynamic Components

---
+++ src/components/ModalDark.vue
@@ -0,0 +1,50 @@
+<template>
+
+	<teleport to=".modals-container">
+		<div 
+			v-if="modelValue"
+			class="modal">
+			<h1>{{ title }}</h1>
+			<slot />
+			<button @click="handleButtonClick()">Hide modal</button>
+		</div>
+	</teleport>
+
+</template>
+
+<script setup>
+// How to use props in Vue 3
+// Note the special modelValue prop
+const props = defineProps({
+	modelValue: {
+		type: Boolean,
+		default: false
+	},
+	title: {
+		type: String,
+		default: 'No title specified'
+	}
+})
+
+// How to use emits
+const emit = defineEmits(['update:modelValue'])
+
+const handleButtonClick = () => {
+	emit('update:modelValue', false)
+}
+</script>
+
+
+<style scoped>
+.modal {
+	background: #333;
+	padding: 10px;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	z-index: 1;
+	color: white;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -1,19 +1,27 @@
 <template>
 	<div class="modals">
 		<h1> Modals </h1>
+		<div>
+			<label>
+				Show dark modals?
+				<input v-model="showDarkModals" type="checkbox" />
+			</label>
+		</div>
 		<button @click="showModal=true">Show Modal</button>
-		<Modal 
+		<component 
+			:is="showDarkModals ? ModalDark : Modal"
 			v-model="showModal"
 			title="My Modal Title vis props">
 			<p>Laboris laboris anim laboris ullamco consequat consequat tempor. Deserunt voluptate culpa esse enim qui Lorem velit culpa amet enim aliquip. Consequat sit velit non cillum aliquip nisi reprehenderit. Dolore ullamco eiusmod Lorem proident do velit labore. Et consectetur qui ut incididunt voluptate anim laboris ex. Aliquip incididunt exercitation laborum adipisicing nulla velit laboris deserunt dolore.</p>
-		</Modal>
+		</component>
 	</div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import Modal from '@/components/Modal.vue'
-
+import ModalDark from '@/components/ModalDark.vue'
+const showDarkModals = ref(false)
 const showModal = ref(false)
 </script>
 
