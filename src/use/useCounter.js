0dc8199caf58d65940ded34f314adec599d88085
flyinacres <ron-github@rfgf.com>
2023-01-27 17:46:30 -0800

    Basic counter composable

---
+++ src/use/useCounter.js
@@ -0,0 +1,47 @@
+// This file is in the standard location
+// The file and function have the standard naming convention
+import {ref, reactive, computed, watch, onMounted, nextTick} from 'vue'
+
+export function useCounter() {
+
+
+	const counterData = reactive({
+		count: 0,
+		title: "My Counter"
+	})
+	
+	// These should be in appropriate sections of code
+	onMounted(() => {
+		console.log('Do Stuff related to counter')
+	})
+	
+	// Note how the data has to be referenced
+	watch(() => counterData.count, (newCount) => {
+		if (newCount === 20) {
+			alert('Your made it to 20!')
+		}
+	})
+	
+	
+	const increaseCounter = amount => {
+		counterData.count += amount
+	
+		// This could also be done using 'await', if the method is declared async
+		nextTick(() => {
+			console.log("Do something after next tick")
+		})
+	}
+	
+	const decreaseCounter = amount => {
+		counterData.count -= amount
+	}
+
+	const oddOrEven = computed(() => {
+		if (counterData.count % 2 === 0) {
+			return "even"
+		}
+		return "odd"
+	})
+
+	return {increaseCounter, decreaseCounter, oddOrEven, counterData}
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,8 +20,13 @@
 </template>
 
 <script setup>
-import {ref, reactive, computed, watch, onMounted, nextTick} from 'vue'
+import {ref, onMounted} from 'vue'
 import { vAutofocus } from '@/directives/vAutofocus'
+import {useCounter} from '@/use/useCounter'
+
+// Note how the data cannot be directly destructured from the import, but must be done 
+// indirectly
+const {increaseCounter, decreaseCounter, oddOrEven, counterData} = useCounter()
 
 // Need to access template refs in this way
 const appTitleRef = ref(null)
@@ -32,46 +37,6 @@ const appTitle = "My OK Counter App"
 onMounted(() => {
 	console.log(`The app title is ${appTitleRef.value.offsetWidth } pixels wide`)
 })
-
-const oddOrEven = computed(() => {
-	if (counterData.count % 2 === 0) {
-		return "even"
-	}
-	return "odd"
-})
-
-
-const counterData = reactive({
-	count: 0,
-	title: "My Counter"
-})
-
-// These should be in appropriate sections of code
-onMounted(() => {
-	console.log('Do Stuff related to counter')
-})
-
-// Note how the data has to be referenced
-watch(() => counterData.count, (newCount) => {
-	if (newCount === 20) {
-		alert('Your made it to 20!')
-	}
-})
-
-
-const increaseCounter = amount => {
-	counterData.count += amount
-
-	// This could also be done using 'await', if the method is declared async
-	nextTick(() => {
-		console.log("Do something after next tick")
-	})
-}
-
-const decreaseCounter = amount => {
-	counterData.count -= amount
-}
-
 
 </script>
 
